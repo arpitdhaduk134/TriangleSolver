a75e80534e43f52f8e044f660ac524059fbd1ece
arpitdhaduk134 <arpitdhaduk134@gmail.com>
2024-03-06 15:32:01 -0500

    Five tests for a valid scalene triangle

--- TriangleSolver/TriangleSolver/TringleSolverTest/UnitTest1.cs
+++ TriangleSolver/TriangleSolver/TringleSolverTest/UnitTest1.cs
@@ -45,5 +45,54 @@ namespace TringleSolverTest
             Assert.AreEqual("Isosceles triangle", result);
         }
 
+        [Test]
+        public void TestScaleneTriangle1()
+        {
+            int A = 2;
+            int B = 3;
+            int C = 4;
+            string result = Triangle.AnalyzeTriangle(A, B, C);
+            Assert.AreEqual("Scalene triangle", result);
+        }
+
+        [Test]
+        public void TestScaleneTriangle2()
+        {
+            int A = 3;
+            int B = 11;
+            int C = 12;
+            string result = Triangle.AnalyzeTriangle(A, B, C);
+            Assert.AreEqual("Scalene triangle", result);
+        }
+
+        [Test]
+        public void TestScaleneTriangle3()
+        {
+            int A = 4;
+            int B = 20;
+            int C = 21;
+            string result = Triangle.AnalyzeTriangle(A, B, C);
+            Assert.AreEqual("Scalene triangle", result);
+        }
+
+        [Test]
+        public void TestScaleneTriangle4()
+        {
+            int A = 3;
+            int B = 5;
+            int C = 7;
+            string result = Triangle.AnalyzeTriangle(A, B, C);
+            Assert.AreEqual("Scalene triangle", result);
+        }
+
+        [Test]
+        public void TestScaleneTriangle5()
+        {
+            int A = 6;
+            int B = 9;
+            int C = 12;
+            string result = Triangle.AnalyzeTriangle(A, B, C);
+            Assert.AreEqual("Scalene triangle", result);
+        }
     }
 }
